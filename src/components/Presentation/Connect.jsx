b81618b1e1ee46a85964e43311c681426a22bcd6
Fauze Cavalari <fauze.cavalari@gmail.com>
2024-02-22 21:11:17 -0300

    Changed cv url download

--- src/components/Presentation/Connect.jsx
+++ src/components/Presentation/Connect.jsx
@@ -18,7 +18,7 @@ export default function Connect(){
         </a>
 
         
-        <a href="https://github.com/devfauze/ReactPortifolio/files/14288616/cv.pdf" download>
+        <a href="https://github.com/devfauze/ReactPortifolio/files/14380594/cv.pdf" download>
             <FaFileDownload className="h-7 w-7 mx-2 my-5 hover:text-red-400 hover:scale-125 duration-200"/>
         </a>
         </section>
